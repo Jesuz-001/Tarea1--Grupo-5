1f92c8591b7e3b3deb0f870313db5a19bccf2216
Jesus David Calderon <davidcalderon2701@gmail.com>
2026-06-27 18:44:21 -0400

    Add files via upload

---
+++ Tarea1CuotaPrestamo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Tarea1CuotaPrestamo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== CALCULO DE CUOTA DE PRESTAMO ===");
+            Console.Write("Monto del prestamo RD$: ");
+            double monto = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Tasa de interes anual %: ");
+            double tasaAnual = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Tiempo en meses: ");
+            int tiempoMeses = Convert.ToInt32(Console.ReadLine());
+            double tasaMensual = (tasaAnual / 100) / 12;
+            double cuota = monto * (tasaMensual / (1 - Math.Pow(1 + tasaMensual, -tiempoMeses)));
+            Console.WriteLine("\n--- Resultado ---");
+            Console.WriteLine("Cuota Mensual: RD$ {0:N2}", cuota);
+            Console.ReadLine();
+        }
+    }
+}
